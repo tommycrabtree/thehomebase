944624eb70ce8aed40581f01f02bd9f859d7538f
Tommy <tommycrabtree@outlook.com>
2023-01-07 00:05:38 -0600

    made controller asnyc

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Dirty", "Filthy", "Pointy", "Gassy", "Thirsty", "Messy", "Quichey", "Gloomy", "Silly", "Douchey"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
