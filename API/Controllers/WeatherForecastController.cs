e5a5fb693085b10a6fa23283ce1ac68f4d8a4001
Tommy <tommycrabtree@outlook.com>
2023-01-11 00:22:15 -0600

    Publish attempt localhost 5001 environment

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Snake Eyes", "Storm Shadow", "Dusty", "Beach Head", "Scarlett", "Lady Jaye", "Wet Suit", "Tomax", "Xamot", "Serpentor"
+        "Optimus", "Bumblebee", "Thundercracker", "Skywarp", "Jazz", "Mirage", "Grapple", "Starscream", "Jetfire", "Sandstorm"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
