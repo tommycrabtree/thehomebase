ce03eacdd763b16997f8bcda134107ffa371496e
Tommy <tommycrabtree@outlook.com>
2023-01-20 13:35:57 -0500

    33 Updated user entity

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddCors();
